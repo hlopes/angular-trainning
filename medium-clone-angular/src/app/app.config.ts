16d91f046adbcd7cd63d5be9241255eabb01acb2
hlopes <hlopes@present-technologies.com>
2023-11-16 09:37:07 +0000

    remove duplicated project

--- medium-clone-angular/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from '../../../medium-clone-angular-15/src/app/app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
-};
